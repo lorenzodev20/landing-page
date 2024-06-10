9a4e72b18cd15226210c76d1440b3118fe6edf15
Lorenzo Antonio Rojo Garces <lorenzodev2020@gmail.com>
2024-06-10 16:30:05 -0500

    Revert "add google tag manager"
    This reverts commit 345ace501b426378acda53494a6cffe5f782fe79.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  runtimeConfig: {
-    public: {
-      gtmContainerId: '', // set NUXT_PUBLIC_GTM_CONTAINER_ID in .env file
-    },
-  },
+  devtools: { enabled: true }
 })
