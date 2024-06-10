345ace501b426378acda53494a6cffe5f782fe79
Lorenzo Antonio Rojo Garces <lorenzodev2020@gmail.com>
2024-06-10 16:18:49 -0500

    add google tag manager

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      gtmContainerId: '', // set NUXT_PUBLIC_GTM_CONTAINER_ID in .env file
+    },
+  },
 })
