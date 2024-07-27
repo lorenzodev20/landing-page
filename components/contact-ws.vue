1acd983f4ed8565201524b394cd9e64c7c352620
Lorenzo Antonio Rojo Garces <lorenzodev2020@gmail.com>
2024-07-27 13:56:03 -0500

    replace mailgun by link to typeform

---
+++ components/contact-ws.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+
+
+</script>
+<template>
+    <!--Begin Contact Form-->
+    <section id="contact" class="about-me bg-white-custom py-5 px-5">
+        <div class="row mt-5 w-100">
+            <div class="col-lg-12">
+                <p class="title-two text-center">¡Hablemos!</p>
+                <!-- <p class="paragraph-two text-center">
+                    Sí tienes una idea de algún proyecto web te puedo asesorar y ayudar a
+                    implementar la solución que tengas en mente.
+                </p> -->
+                <div class="container-contact">
+                    <div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+                        <p class="paragraph-two text-center" style="font-size:20px">
+                            Como desarrollador web freelancer, puedo ayudarte a llevar tu visión a la vida. Ya sea una tienda en línea, un sitio web corporativo o una aplicación web, estoy aquí para asesorarte y ayudarte a implementar la solución perfecta. No esperes más para ver resultados.
+                        </p>
+                        <p class="paragraph-two text-center" style="font-size:20px">
+                            Completa los datos del siguiente formulario y me estaré comunicando contigo a la mayor brevedad posible. 
+                        </p>
+                        <p class="paragraph-two text-center" style="font-size:20px">
+                            Haz click &#128073 <a
+                            href="https://z7mha97w7wb.typeform.com/to/lQGnNZB9"
+                            target="_blank"
+                            style="text-decoration: none; font-weight: bold;"
+                            >aquí</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--End Contact Form-->
+</template>
+
+<style>
+.swal-text {
+    text-align: center;
+}
+
+.swal-footer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.spinner-custom {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+}
+
+.spinner-custom-item {
+    display: flex;
+    justify-content: center;
+}
+</style>
--- components/contact.vue
+++ components/contact.vue
@@ -1,50 +1,39 @@
 <script setup lang="ts">
-const { checkToken, emailSend } = useLanding()
+import swal from 'sweetalert';
+
+//const { checkToken, emailSend } = useLanding()
+const config = useRuntimeConfig();
 const sendingEmail = ref(true)
-const name = ref(null)
-const email = ref(null)
-const subject = ref(null)
-const message = ref(null)
+const name = ref('Pepito Perez')
+const email = ref('yosoypepito@email.com')
+const subject = ref('Contact Form LorenzoDev')
+const message = ref(`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`)
+const { client } = useMailgun();
 
-// onMounted(() => {
-//     localStorage.clear()
-//     checkToken()
-// })
-// onUpdated(() => {
-//     checkToken()
-// })
+const sendEmail = async (event: any) => {
+    event.preventDefault();
+    sendingEmail.value = false
+    const mg = await client();
+    const response = await mg.messages.create(config.mailgun.domain, {
+        from: `${name.value} <${email.value}>`,
+        to: 'solucionesinformaticas960@gmail.com',
+        subject: `${subject.value}`,
+        text: `${message.value}`,
+    });
+    console.log(response)
 
-// const dataForEmail = computed(() => ({
-//     get: () => ({
-//         name: name.value,
-//         email: email.value,
-//         subject: subject.value,
-//         message: message.value
-//     }),
-//     set: (value) => {
-//         name.value = ''
-//         email.value = ''
-//         subject.value = ''
-//         message.value = ''
-//     }
-// }))
+    console.log('Enviando')
+    // setTimeout(() => {
+    //     console.log('Enviado')
+    //     sendingEmail.value = true
+    //     return swal('Mensaje enviado', 'Gracias por contactarme, estaré respondiendo su mensaje a la mayor brevedad posible.', 'success')
+    // }, 2000);
+}
 
-// const sendEmail = async () => {
-//     sendingEmail.value = false
-//     const { name, email, subject, message } = dataForEmail.value.get()
-//     let response = await emailSend({ name, email, subject, message })
-//     if (!response.rps) {
-//         sendingEmail.value = true
-//         return swal('Oops', response.message, 'warning')
-//     }
-//     dataForEmail.value.set()
-//     sendingEmail.value = true
-//     return swal('Mensaje enviado', 'Gracias por contactarme, estaré respondiendo su mensaje a la mayor brevedad posible.', 'success')
-// }
 </script>
 <template>
     <!--Begin Contact Form-->
-    <section id="contact" class="about-me bg-white-custom py-5 px-5" @submit.prevent="sendEmail">
+    <section id="contact" class="about-me bg-white-custom py-5 px-5">
         <div class="row mt-5 w-100">
             <div v-if="!sendingEmail" class="spinner-custom">
                 <div class="spinner-custom-item"><img src="../assets/spinners/spinner-1s-200px.svg" alt="loading"></div>
@@ -59,7 +48,7 @@ const message = ref(null)
                     implementar la solución que tengas en mente.
                 </p>
                 <div class="container-contact">
-                    <form>
+                    <form @submit.prevent="sendEmail">
                         <div class="mb-3">
                             <input type="text" class="form-control" id="name" v-model="name" placeholder="Tu nombre" />
                         </div>
@@ -80,6 +69,9 @@ const message = ref(null)
                             <textarea name="message" class="form-control" id="message" cols="20" rows="10"
                                 placeholder="Escribe tu mensaje" v-model="message"></textarea>
                         </div>
+                        <!-- <button v-if="sendingEmail" type="submit" class="btn btn btn-primary btn-lg btn-custom">
+                            Envía tu mensaje
+                        </button> -->
                         <button v-if="sendingEmail" type="submit" class="btn btn btn-primary btn-lg btn-custom">
                             Envía tu mensaje
                         </button>
